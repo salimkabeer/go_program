bb9940f2a7757150ac65fb769e48032ac3271697
salimkabeer <salimkabeer@gmail.com>
2019-03-23 08:56:30 +0530

    read me check

--- HelloWord.go
+++ HelloWord.go
@@ -1,6 +1,7 @@
 /* It defines the pkg name in which program should lie, Its a mandatory stmt.
 As Go run in pkg, the main pkg is the starting point to run the program. 
 Each pkg has a path and name associated with it*/
+
 package main
 
 import "fmt" //It's a preprocessor command to include pkg "fmt"
